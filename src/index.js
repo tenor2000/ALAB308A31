91b149d14c26e2f491b5aded577cfa609a769529
tenor2000 <tenor2000@gmail.com>
2025-03-25 15:21:43 -0400

    enabled to see results in html live server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-console.time("Overall");
+console.time("Overall Time");
 
 function getUserData(id) {
+  console.time("Request time for ID " + id);
   const dbs = {
     db1: db1,
     db2: db2,
@@ -17,6 +18,7 @@ function getUserData(id) {
 
   return Promise.all([dbData, vaultData])
     .then(([dbObj, vaultObj]) => {
+      console.timeEnd("Request time for ID " + id);
       return {
         id: id,
         name: vaultObj.name,
@@ -29,8 +31,9 @@ function getUserData(id) {
       };
     })
     .catch((error) => {
-      console.log("Error: ", error);
-      return null;
+      console.log(error);
+      console.timeEnd("Overall time for ID " + id);
+      return "Error";
     });
 
   // helper function
@@ -47,17 +50,29 @@ const usersQuery = [
   getUserData(1),
   getUserData(2),
   getUserData(3),
+  getUserData(4),
+  getUserData(5),
   getUserData(6),
   getUserData(7),
   getUserData(8),
   getUserData(9),
+  getUserData(10),
+  getUserData(true),
+  getUserData(11),
 ];
 const userData = await Promise.all(usersQuery).then((results) => {
   return results;
 });
 
+const app = document.getElementById("app");
+const timer = document.createElement("h2");
+timer.textContent = "Check console for times stamps";
+app.appendChild(timer);
+
 userData.forEach((person) => {
-  console.log(person);
+  const pre = document.createElement("pre");
+  pre.textContent = JSON.stringify(person, null, 2);
+  app.appendChild(pre);
 });
 
-console.timeEnd("Overall");
+console.timeEnd("Overall Time");
